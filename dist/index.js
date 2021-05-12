5621d7426716d25bf36c20bb26989aaf3b13410a
陈远翔 <cyxhuan9@163.com>
2021-05-12 15:06:53 +0800

    build

--- dist/index.js
+++ dist/index.js
@@ -13,13 +13,13 @@ try {
   const commits = payload.commits
     .map(c => c.message)
     .filter(m => !/^Merge pull request/.test(m))
-    .map(m => m.replace(/\s+/g, ' '))
+    .map(m => m.replace(/\n+(.*)/g, '\n> $1'))
     .map(m => `> ${m}`)
     .join('\n')
 
   core.setOutput('commits', commits)
 } catch (error) {
-  core.setFailed(error.message)
+  core.error(error.message)
 }
 
 
